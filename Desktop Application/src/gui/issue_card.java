09ff5924977d905cc8935d1bc640d88da8160d4c
MadushaShanaka <tkmshanaka@gmail.com>
2021-02-01 23:53:14 +0530

    Adding some user friendly views

--- Desktop Application/src/gui/issue_card.java
+++ Desktop Application/src/gui/issue_card.java
@@ -22,32 +22,33 @@ public class issue_card {
      URL url = new URL("https://httpbin.org/post");
 
             StringBuilder postData = new StringBuilder();
-            postData.append(URLEncoder.encode("username", "UTF-8"));
+            postData.append(URLEncoder.encode("first_name", "UTF-8"));
 	    postData.append('=');
-	    postData.append(URLEncoder.encode((String) uname, "UTF-8"));
+	    postData.append(URLEncoder.encode((String) fname, "UTF-8"));
             
             postData.append('&');
-            postData.append(URLEncoder.encode("passward", "UTF-8"));
+            postData.append(URLEncoder.encode("last_name", "UTF-8"));
 	    postData.append('=');
-	    postData.append(URLEncoder.encode((String) pward, "UTF-8"));
+	    postData.append(URLEncoder.encode((String) lname, "UTF-8"));
             
             postData.append('&');
-            postData.append(URLEncoder.encode("passward", "UTF-8"));
+            postData.append(URLEncoder.encode("amount", "UTF-8"));
 	    postData.append('=');
-	    postData.append(URLEncoder.encode((String) pward, "UTF-8"));
+	    postData.append(URLEncoder.encode((String) amount, "UTF-8"));
             
             postData.append('&');
-            postData.append(URLEncoder.encode("passward", "UTF-8"));
+            postData.append(URLEncoder.encode("card_id", "UTF-8"));
 	    postData.append('=');
-	    postData.append(URLEncoder.encode((String) pward, "UTF-8"));
+	    postData.append(URLEncoder.encode((String) card_id, "UTF-8"));
             
             postData.append('&');
-            postData.append(URLEncoder.encode("passward", "UTF-8"));
+            postData.append(URLEncoder.encode("employer_id", "UTF-8"));
 	    postData.append('=');
-	    postData.append(URLEncoder.encode((String) pward, "UTF-8"));
+	    postData.append(URLEncoder.encode((String) employer_id, "UTF-8"));
             
 	    byte[] postDataBytes = postData.toString().getBytes("UTF-8");
 	    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestProperty("Authorization", "admin2021");
 	    conn.setRequestMethod("POST");
 	    conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 	    conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
@@ -64,8 +65,8 @@ public class issue_card {
 	    System.out.println("origin- "+myResponse.getString("origin"));
 	    System.out.println("url- "+myResponse.getString("url"));
 	    JSONObject form_data = myResponse.getJSONObject("form");
-	    System.out.println("Username- "+form_data.getString("username"));
-	    System.out.println("Passward- "+form_data.getString("passward"));
+	    System.out.println("First name- "+form_data.getString("first_name"));
+	    System.out.println("Last name- "+form_data.getString("last_name"));
    }
 
 }
