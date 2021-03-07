dfcd01e1d72e78498f778b1ff14b179a3db97eab
Nadun Welikanda <nadunwelikanda.360@gmail.com>
2021-03-08 01:32:09 +0530

    undo changes

--- Backends/Online Server/src/index.js
+++ Backends/Online Server/src/index.js
@@ -1,29 +1,7 @@
 'use strict'
 
-const config = require('./config')
-// const app = require('./services/express')
+const app = require('./services/express')
 const mysql = require('./services/mysql')
-
-
-const express = require('express')
-const bodyParser = require('body-parser')
-const errorHandler = require('./middlewares/error-handler')
-const cors = require('cors')
-const api = require('./routes/api')
-const port = (config.port) ? config.port : 4000
-const app = express()
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
-
-app.use(cors())
-
-// Route of the app will be done in here
-app.use('/', api)
-app.use(errorHandler.handleNotFound)
-app.use(errorHandler.handleError)
 
 ///////////////////////// TESTING AREA START ///////////////////
 
@@ -36,17 +14,9 @@ app.use(errorHandler.handleError)
 
 
 // Start app and connect it to the database
-// app.start()
+app.start()
 mysql.connect()
 
-app.listen(port, (err) => {
-    if (err) {
-      console.log(`Error : ${err}`)
-      process.exit(-1)
-    }
-
-    console.log(`${config.app} is running on port ${port}`)
-});
 
 /////////////////////// TESTING AREA START /////////////////////////
 
